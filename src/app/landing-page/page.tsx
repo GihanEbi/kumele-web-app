4c250c669e1d178d667d27e9b1b5e43f72974dad
Gihan Alwis <ebi011@jithpl.com>
2025-05-26 21:19:50 +0530

    setup the landing page

--- src/app/landing-page/page.tsx
+++ src/app/landing-page/page.tsx
@@ -17,9 +17,41 @@ const LandingPge = () => {
         className="z-0 opacity-50" // Adjust opacity as needed
       />
       {/* header section of landing page */}
-      <div className="flex flex-col">
-        <div>hello</div>
-        <div>world</div>
+      <div className="flex flex-row mt-3">
+        <div className="flex-1/3 ml-1.5 gap-2">
+          <Image
+            className="p-2"
+            alt="logo"
+            src="/images/logo.png"
+            width={63}
+            height={50}
+          />
+        </div>
+        <div className="flex-2/3 text-2xl font-bold">House Party</div>
+      </div>
+      {/* avatar section  */}
+      <div>
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
+
+      {/* body section */}
+      <div>
+        <div>body img</div>
+        <div>Hobby Meetups</div>
+        <div>"Let your Hobbies define your character" -G.Baku</div>
+        <div>We play.We overcome. We experience.</div>
+
+        <div className="flex flex-row justify-center">
+          <div className="flex-1">
+            <Button>Sign in</Button>
+          </div>
+          <div className="flex-1">
+            <Button>Signup</Button>
+          </div>
+        </div>
       </div>
     </div>
   );
