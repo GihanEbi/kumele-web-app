8ac1ae9d7197ea5745f7027df0a9c75d8ff53727
Gihan Alwis <ebi011@jithpl.com>
2025-09-15 19:22:52 +0530

    subscription routes createed

---
+++ src/routes/subscription/index.ts
@@ -0,0 +1,85 @@
+import { config } from "@/config";
+import { getToken } from "@/utils/authUtils";
+
+// -------types----------
+type createSubscriptionForm = {
+  subscription_id: string;
+  stripe_payment_intent_id: string;
+};
+
+const commonUrl = `${config.baseUrl}/subscriptions`;
+
+// create user subscription
+export const createUserSubscription = async (
+  dataObj: createSubscriptionForm
+) => {
+  try {
+    const res = await fetch(`${commonUrl}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `${getToken()}`,
+      },
+      body: JSON.stringify(dataObj),
+    });
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+// get all subscription data
+export async function get_all_subscriptions() {
+  try {
+    const res = await fetch(`${commonUrl}/get-all-subscription-data`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `${getToken()}`, // Ensure getToken() returns a valid token
+      },
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+}
+
+// get user subscription data
+export async function get_user_subscription_data() {
+  try {
+    const res = await fetch(`${commonUrl}/get-all-user-subscriptions`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `${getToken()}`, // Ensure getToken() returns a valid token
+      },
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+}
+
+// deactivate user subscription
+export async function deactivate_user_subscription(subscriptionId: string) {
+  try {
+    const res = await fetch(
+      `${commonUrl}/deactivate-user-subscription/${subscriptionId}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `${getToken()}`, // Ensure getToken() returns a valid token
+        },
+      }
+    );
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+}
