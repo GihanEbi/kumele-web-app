547587d0db945a84107df635eb5b255d932985b3
GayanGamage96 <gayangamagercg1996@gmail.com>
2026-07-07 09:29:28 +0530

    feat: testing purpose only

--- src/app/user/blog/page.tsx
+++ src/app/user/blog/page.tsx
@@ -91,6 +91,9 @@ const Blog: React.FC = () => {
     }
   }, [categories]);
 
+  //git version testing
+  //testing codess  
+
   // Mobile-like drag scrolling handlers
   const handleMouseDown = (e: React.MouseEvent) => {
     setIsDragging(true);
